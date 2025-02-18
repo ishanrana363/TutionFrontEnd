44c3d35933a879c5ecc7f860f3cd58f6e3df95ee
ishanrana363 <ishanrana363@gmail.com>
2025-02-18 19:33:17 +0600

    name page design completed

--- src/components/tutor/setting/Name.jsx
+++ src/components/tutor/setting/Name.jsx
@@ -5,7 +5,7 @@ const Name = () => {
         <div>
             <h1 className=' text-[#141414] font-bold text-xl ' >Update Name</h1>
             <div className='my-6' >
-                <input type="text" name="" className='bg-white px-4 py-2 rounded-xl shadow focus:outline-0 ' id="" />
+                <input placeholder='Ishan' type="text" name="" className='bg-white w-[50%] px-4 py-2 rounded-xl shadow focus:outline-0 ' id="" />
             </div>
         </div>
     );
