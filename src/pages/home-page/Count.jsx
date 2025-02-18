19b12ea8bb5aa604c45d96bd9f15ce6d65059903
ishanrana363 <ishanrana363@gmail.com>
2025-02-18 21:49:41 +0600

    count page design completed

---
+++ src/pages/home-page/Count.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Count = () => {
+    return (
+        <div className='bg-[#64A8E8] py-5 ' >
+            <div className='flex max-w-6xl mx-auto justify-between ' >
+                <div>
+                    <h1 className=' text-4xl text-center text-white font-bold ' >200+</h1>
+                    <p className=' text-[#FFFFFF] ' >Qualified Tutors</p>
+                </div>
+                <div>
+                    <h1 className=' text-4xl text-center text-white font-bold ' >500+</h1>
+                    <p className=' text-[#FFFFFF] ' >Successful Matches</p>
+                </div>
+                <div>
+                    <h1 className=' text-4xl text-center text-white font-bold ' >95%</h1>
+                    <p className=' text-[#FFFFFF] ' >Satisfaction Rate</p>
+                </div>
+                <div>
+                    <h1 className=' text-4xl text-center text-white font-bold ' >50+</h1>
+                    <p className=' text-[#FFFFFF] '  >Subject Available</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Count;
--- src/pages/home-page/HomePages.jsx
+++ src/pages/home-page/HomePages.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Banner from './Banner'
+import Count from './Count'
 
 const HomePages = () => {
   return (
     <div>
       <Banner></Banner>
+      <Count></Count>
     </div>
   )
 }
